bdca9c673e8294a4a8cc4a5b469ff66f2f8ac93d
Reyajosephine <reyacelene@example.com>
2024-10-18 22:58:11 +0530

    new

--- detail-script.js
+++ detail-script.js
@@ -13,29 +13,35 @@ async function fetchMovieDetails(movieId) {
     const response = await fetch(
       `https://guvi-book-my-show-backend.onrender.com/movie/${movieId}`
     );
+
+    if (!response.ok) {
+      const errorMessage = await response.text(); // Get error message from response
+      console.error("Failed to fetch movie details:", errorMessage);
+      alert("Failed to load movie details.");
+      return; // Stop execution if there's an error
+    }
+
     const movie = await response.json();
 
-    if (response.ok) {
-      const movieDetailsContainer = document.getElementById("movieDetails");
-      movieDetailsContainer.innerHTML = `<div style="color: #fff; padding: 0px">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-4">
-                <img
-                  src="${movie.image}"
-                  alt=""
-                  class="img-fluid mb-4"
-                  style="max-width: 100%; height: 100%; object-fit: cover"
-                />
-              </div>
-              <div class="col-md-8">
-                <div style="color: black; padding: 0px">
-                  <h1 class="mb-4">${movie.title}</h1>
-                  <h4 class="mb-4">Director : ${movie.director}</h4>
-                  <h4 class="mb-4">Genre : ${movie.genre}</h4>
-                  <h2>About the movie</h2>
-                  <p>${movie.description}</p>
-                </div>
+    const movieDetailsContainer = document.getElementById("movieDetails");
+    movieDetailsContainer.innerHTML = `<div style="color: #fff; padding: 0px">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <img
+                src="${movie.image}"
+                alt=""
+                class="img-fluid mb-4"
+                style="max-width: 100%; height: 100%; object-fit: cover"
+              />
+            </div>
+            <div class="col-md-8">
+              <div style="color: black; padding: 0px">
+                <h1 class="mb-4">${movie.title}</h1>
+                <h4 class="mb-4">Director : ${movie.director}</h4>
+                <h4 class="mb-4">Genre : ${movie.genre}</h4>
+                <h2>About the movie</h2>
+                <p>${movie.description}</p>
               </div>
             </div>
           </div>
@@ -44,64 +50,60 @@ async function fetchMovieDetails(movieId) {
       <hr style="margin-top: 50px" />
       <h2 class="mt-4 mb-4 text-center">Book Tickets</h2>`;
 
-      const showContainer = document.createElement("div");
+    const showContainer = document.createElement("div");
 
-      showContainer.innerHTML = `
-                <div class="container mt-4">
-                    <div class="table-responsive">
-                        <table class="table table-bordered">
-                            <thead>
+    showContainer.innerHTML = `
+            <div class="container mt-4">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Showtime</th>
+                                <th>Seats</th>
+                                <th>Book Tickets</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            ${Object.entries(movie.shows)
+                              .flatMap(([date, showtimes]) =>
+                                showtimes.map(
+                                  (show, index) => `
                                 <tr>
-                                    <th>Date</th>
-                                    <th>Showtime</th>
-                                    <th>Seats</th>
-                                    <th>Book Tickets</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                ${Object.entries(movie.shows)
-                                  .flatMap(([date, showtimes]) =>
-                                    showtimes.map(
-                                      (show, index) => `
-                                    <tr>
-                                        ${
-                                          index === 0
-                                            ? `<td rowSpan="${showtimes.length}" class="align-middle"><strong>${date}</strong></td>`
-                                            : ""
-                                        }
-                                        <td>
-                                            <div class="border p-1" style="display: flex; flex-direction: column; align-items: center;">
-                                                <div>${show.time}</div>
-                                                <div class="text-muted" style="font-size: 15px;">4K Dolby Atmos 7.1</div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="border p-3" style="display: flex; flex-direction: column; align-items: center;">
-                                                <div>${show.seats}</div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <!-- Button to book tickets -->
-                                            <button class="btn btn-sm btn-danger p-3 d-block w-100" onclick="bookTickets('${
-                                              movie._id
-                                            }', '${
-                                        show.id
-                                      }', '${date}')">Book Tickets</button>
-                                        </td>
-                                    </tr>`
-                                    )
-                                  )
-                                  .join("")}
-                            </tbody>
-                        </table>
-                    </div>
+                                    ${
+                                      index === 0
+                                        ? `<td rowSpan="${showtimes.length}" class="align-middle"><strong>${date}</strong></td>`
+                                        : ""
+                                    }
+                                    <td>
+                                        <div class="border p-1" style="display: flex; flex-direction: column; align-items: center;">
+                                            <div>${show.time}</div>
+                                            <div class="text-muted" style="font-size: 15px;">4K Dolby Atmos 7.1</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="border p-3" style="display: flex; flex-direction: column; align-items: center;">
+                                            <div>${show.seats}</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <!-- Button to book tickets -->
+                                        <button class="btn btn-sm btn-danger p-3 d-block w-100" onclick="bookTickets('${
+                                          movie._id
+                                        }', '${
+                                      show.id
+                                    }', '${date}')">Book Tickets</button>
+                                    </td>
+                                </tr>`
+                                )
+                              )
+                              .join("")}
+                        </tbody>
+                    </table>
                 </div>
-            `;
-      document.body.appendChild(showContainer);
-    } else {
-      console.error("Failed to fetch movie details:", movie);
-      alert("Failed to load movie details.");
-    }
+            </div>
+        `;
+    document.body.appendChild(showContainer);
   } catch (error) {
     console.error("Error fetching movie details:", error);
     alert("Something went wrong while fetching movie details.");
@@ -119,7 +121,7 @@ document.getElementById("submitBooking").addEventListener("click", async () => {
 
   try {
     const response = await fetch(
-      "https://guvi-book-my-show-backend.onrender.com/book-ticket", // Updated endpoint for booking tickets
+      "https://guvi-book-my-show-backend.onrender.com/book-tickets", // Updated endpoint for booking tickets
       {
         method: "POST",
         headers: {
@@ -135,22 +137,23 @@ document.getElementById("submitBooking").addEventListener("click", async () => {
         }),
       }
     );
-    const result = await response.json();
 
-    if (response.ok) {
-      const bookingModal = bootstrap.Modal.getInstance(
-        document.getElementById("bookingModal")
-      );
-      bookingModal.hide();
-      alert("Tickets booked successfully!");
-      window.location.reload();
-    } else {
-      console.error("Booking failed:", result);
+    if (!response.ok) {
+      const errorMessage = await response.text(); // Get error message from response
+      console.error("Failed to book tickets:", errorMessage);
       alert("Failed to book tickets. Please try again.");
+      return; // Stop execution if there's an error
     }
+
+    const bookingModal = bootstrap.Modal.getInstance(
+      document.getElementById("bookingModal")
+    );
+    bookingModal.hide();
+    alert("Tickets booked successfully!");
+    window.location.reload();
   } catch (error) {
-    console.error("Error booking tickets:", error);
-    alert("Something Went Wrong!");
+    console.error("Error during ticket booking:", error);
+    alert("Something went wrong while booking tickets.");
   }
 });
 
