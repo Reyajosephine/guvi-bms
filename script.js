0f8608c2534eecb32cebadb0a6c2e05b78c54868
Reyajosephine <reyacelene@example.com>
2024-10-20 18:48:13 +0530

    new

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 async function fetchMovies() {
   try {
-    const response = await fetch("https://booking-app-db.onrender.com/movie/get-movies");
+    const response = await fetch("https://guvi-book-my-show-backend-4.onrender.com/movie/get-movies");
     const movies = await response.json();
     const movieCardsContainer = document.getElementById("movieCards");
     movieCardsContainer.innerHTML = "";
