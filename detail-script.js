4965be39804cb00f5aee8e1f04b2378960165c9a
Reyajosephine <reyacelene@example.com>
2024-10-20 20:29:55 +0530

    new

--- detail-script.js
+++ detail-script.js
@@ -119,7 +119,7 @@ document.getElementById("submitBooking").addEventListener("click", async () => {
 
   try {
     const response = await fetch(
-      "http://localhost:7000/movie/book-movie",
+      "https://learn-with-guvi-book-my-show-backend.onrender.com/movie/book-movie",
       {
         method: "POST",
         headers: {
